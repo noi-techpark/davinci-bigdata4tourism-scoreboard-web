92fc18a470e1e1f9df62b161b369db51b09a9c99
gappc <gapp.christian@gmail.com>
2019-12-08 17:30:53 +0100

    Add line chart to accomodation metric

--- components/scoreboard/accomodation-category.vue
+++ components/scoreboard/accomodation-category.vue
@@ -1,12 +1,15 @@
 <template>
   <StatsContainer>
     <GaugeList :gauges="gauges"></GaugeList>
+    <LineChart :chartData="chartData" :options="chartOptions"></LineChart>
   </StatsContainer>
 </template>
 
 <script>
 import provideDataMixin from './mixins/scoreboard-data.mixin'
+import { rgbaPalette } from '@/components/charts/color-util'
 import GaugeList from '@/components/charts/gauge-list.vue'
+import LineChart from '@/components/charts/year.vue'
 import StatsContainer from '@/components/stats-container.vue'
 
 import * as esConfig from '@/meta/elasticsearch/accomodation-category'
@@ -17,10 +20,71 @@ const percentage = (total, value) => (total !== 0 ? (100.0 / total) * value : 0)
 export default {
   components: {
     GaugeList,
+    LineChart,
     StatsContainer
   },
   mixins: [provideDataMixin(esConfig, filters.applyQueryFilters)],
   computed: {
+    chartData() {
+      if (this.metric.results == null) {
+        return null
+      }
+
+      const colors = rgbaPalette(5, 0.5)
+
+      const dateHistogram = this.metric.results[0][esConfig.propDateHistogram]
+      return {
+        labels: dateHistogram.labels,
+        datasets: [
+          {
+            label: 'Hotels 1-3 stars',
+            steppedLine: true,
+            data: dateHistogram[esConfig.propHotel1to3],
+            borderColor: colors[0],
+            fill: false
+          },
+          {
+            label: 'Hotels 4-5 stars',
+            steppedLine: true,
+            data: dateHistogram[esConfig.propHotel4to5],
+            borderColor: colors[1],
+            fill: false
+          },
+          {
+            label: 'Private accomodations',
+            steppedLine: true,
+            data: dateHistogram[esConfig.propPrivate],
+            borderColor: colors[2],
+            fill: false
+          },
+          {
+            label: 'Farms',
+            steppedLine: true,
+            data: dateHistogram[esConfig.propFarms],
+            borderColor: colors[3],
+            fill: false
+          },
+          {
+            label: 'Others',
+            steppedLine: true,
+            data: dateHistogram[esConfig.propOthers],
+            borderColor: colors[4],
+            fill: false
+          }
+        ]
+      }
+    },
+    chartOptions() {
+      return {
+        maintainAspectRatio: false,
+        responsive: true,
+
+        title: {
+          display: true,
+          text: this.metric.title
+        }
+      }
+    },
     gauges() {
       if (this.metric.results == null) {
         return []
@@ -32,6 +96,18 @@ export default {
         {
           title: 'Total',
           value: total
+        },
+        {
+          decimalPlaces: 2,
+          title: 'Hotel 1-3 stars',
+          value: percentage(total, result[esConfig.propHotel1to3].value),
+          unit: '%'
+        },
+        {
+          decimalPlaces: 2,
+          title: 'Hotel 4-5 stars',
+          value: percentage(total, result[esConfig.propHotel4to5].value),
+          unit: '%'
         },
         {
           decimalPlaces: 2,
@@ -49,18 +125,6 @@ export default {
           decimalPlaces: 2,
           title: 'Others',
           value: percentage(total, result[esConfig.propOthers].value),
-          unit: '%'
-        },
-        {
-          decimalPlaces: 2,
-          title: 'Hotel 1-3 stars',
-          value: percentage(total, result[esConfig.propHotel1to3].value),
-          unit: '%'
-        },
-        {
-          decimalPlaces: 2,
-          title: 'Hotel 4-5 stars',
-          value: percentage(total, result[esConfig.propHotel4to5].value),
           unit: '%'
         }
       ]
--- meta/elasticsearch/accomodation-category.js
+++ meta/elasticsearch/accomodation-category.js
@@ -1,9 +1,14 @@
+import dayjs from 'dayjs'
+
 export const propFarms = 'farms'
 export const propHotel1to3 = 'hotel1to3'
 export const propHotel4to5 = 'hotel4to5'
 export const propPrivate = 'private'
 export const propOthers = 'others'
 export const propTotal = 'total'
+export const propDateHistogram = 'dateHistogram'
+
+const propByType = 'byType'
 
 export const query = {
   aggs: {
@@ -37,6 +42,57 @@ export const query = {
           }
         }
       }
+    },
+    [propDateHistogram]: {
+      date_histogram: {
+        field: 'submitted_on',
+        interval: '1w',
+        time_zone: 'Europe/Berlin',
+        min_doc_count: 1
+      },
+      aggs: {
+        [propByType]: {
+          filters: {
+            filters: {
+              [propFarms]: {
+                query_string: {
+                  query: 'category.name:"agriturismi"',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              },
+              [propHotel1to3]: {
+                query_string: {
+                  query: 'category.name:"hotel 1-3 stelle"',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              },
+              [propHotel4to5]: {
+                query_string: {
+                  query: 'category.name:"hotel 4-5 stelle"',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              },
+              [propPrivate]: {
+                query_string: {
+                  query: 'category.name:"accomodations privati"',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              },
+              [propOthers]: {
+                query_string: {
+                  query: 'category.name:"altro"',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              }
+            }
+          }
+        }
+      }
     }
   },
   size: 0
@@ -60,5 +116,27 @@ export const resultBuilder = (response) => ({
   },
   [propTotal]: {
     value: response.hits.total
-  }
+  },
+  [propDateHistogram]: (() => {
+    const result = {
+      labels: [],
+      [propFarms]: [],
+      [propHotel1to3]: [],
+      [propHotel4to5]: [],
+      [propPrivate]: [],
+      [propOthers]: []
+    }
+    response.aggregations[propDateHistogram].buckets.map((bucket) => {
+      const buckets = bucket[propByType].buckets
+      const label = dayjs(bucket.key).format('DD.MM.YYYY')
+
+      result.labels.push(label)
+      result[propFarms].push(buckets[propFarms].doc_count)
+      result[propHotel1to3].push(buckets[propHotel1to3].doc_count)
+      result[propHotel4to5].push(buckets[propHotel4to5].doc_count)
+      result[propPrivate].push(buckets[propPrivate].doc_count)
+      result[propOthers].push(buckets[propOthers].doc_count)
+    })
+    return result
+  })()
 })
