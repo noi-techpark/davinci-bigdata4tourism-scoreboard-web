864a785bfa7f66e7763520ced9ac027091a0bc48
gappc <gapp.christian@gmail.com>
2019-12-08 18:29:18 +0100

    Add stacked bar chart to family metric

--- components/scoreboard/family-situation.vue
+++ components/scoreboard/family-situation.vue
@@ -1,11 +1,14 @@
 <template>
   <StatsContainer>
     <GaugeList :gauges="gauges"></GaugeList>
+    <BarChart :chartData="chartData" :options="chartOptions"></BarChart>
   </StatsContainer>
 </template>
 
 <script>
 import provideDataMixin from './mixins/scoreboard-data.mixin'
+import { rgbaPalette } from '@/components/charts/color-util'
+import BarChart from '@/components/charts/bar.vue'
 import GaugeList from '@/components/charts/gauge-list.vue'
 import StatsContainer from '@/components/stats-container.vue'
 
@@ -16,11 +19,92 @@ const percentage = (total, value) => (total !== 0 ? (100.0 / total) * value : 0)
 
 export default {
   components: {
+    BarChart,
     GaugeList,
     StatsContainer
   },
   mixins: [provideDataMixin(esConfig, filters.applyQueryFilters)],
   computed: {
+    chartData() {
+      if (this.metric.results == null) {
+        return null
+      }
+
+      const colors = rgbaPalette(5, 0.5)
+      const hoverColors = rgbaPalette(5, 0.8)
+
+      const dateHistogram = this.metric.results[0][esConfig.propDateHistogram]
+      return {
+        labels: dateHistogram.labels,
+        datasets: [
+          {
+            label: 'Unaccompanied',
+            data: dateHistogram[esConfig.propUnaccompanied],
+            backgroundColor: colors[0],
+            borderColor: colors[0],
+            hoverBackgroundColor: hoverColors[0],
+            fill: false
+          },
+          {
+            label: 'Groups without children',
+            data: dateHistogram[esConfig.propGroupsWithoutChildren],
+            backgroundColor: colors[1],
+            borderColor: colors[1],
+            hoverBackgroundColor: hoverColors[1],
+            fill: false
+          },
+          {
+            label: 'Couples without children',
+            data: dateHistogram[esConfig.propCouplesWithoutChildren],
+            backgroundColor: colors[2],
+            borderColor: colors[2],
+            hoverBackgroundColor: hoverColors[2],
+            fill: false
+          },
+          {
+            label: 'Singles with children',
+            data: dateHistogram[esConfig.propSinglesWithChildren],
+            backgroundColor: colors[3],
+            borderColor: colors[3],
+            hoverBackgroundColor: hoverColors[3],
+            fill: false
+          },
+          {
+            label: 'Families',
+            data: dateHistogram[esConfig.propFamilies],
+            backgroundColor: colors[4],
+            borderColor: colors[4],
+            hoverBackgroundColor: hoverColors[4],
+            fill: false
+          }
+        ]
+      }
+    },
+    chartOptions() {
+      return {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          xAxes: [
+            {
+              stacked: true
+            }
+          ],
+          yAxes: [
+            {
+              stacked: true,
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        },
+        title: {
+          display: true,
+          text: this.metric.title
+        }
+      }
+    },
     gauges() {
       if (this.metric.results == null) {
         return []
--- meta/elasticsearch/family-situation.js
+++ meta/elasticsearch/family-situation.js
@@ -1,9 +1,13 @@
+import dayjs from 'dayjs'
 export const propUnaccompanied = 'unaccompanied'
 export const propCouplesWithoutChildren = 'couplesWithoutChildren'
 export const propGroupsWithoutChildren = 'groupsWithoutChildren'
 export const propSinglesWithChildren = 'singlesWithChildren'
 export const propFamilies = 'families'
 export const propTotal = 'total'
+export const propDateHistogram = 'dateHistogram'
+
+const propByType = 'byType'
 
 export const query = {
   aggs: {
@@ -47,6 +51,57 @@ export const query = {
           }
         }
       }
+    },
+    [propDateHistogram]: {
+      date_histogram: {
+        field: 'submitted_on',
+        interval: '1w',
+        time_zone: 'Europe/Berlin',
+        min_doc_count: 1
+      },
+      aggs: {
+        [propByType]: {
+          filters: {
+            filters: {
+              [propUnaccompanied]: {
+                query_string: {
+                  query: 'adults:1 AND children:0',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              },
+              [propCouplesWithoutChildren]: {
+                query_string: {
+                  query: 'adults:2 AND children:0',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              },
+              [propGroupsWithoutChildren]: {
+                query_string: {
+                  query: 'adults:>2 AND children:0',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              },
+              [propSinglesWithChildren]: {
+                query_string: {
+                  query: 'adults:1 AND children:>0',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              },
+              [propFamilies]: {
+                query_string: {
+                  query: 'adults:>1 AND children:>0',
+                  analyze_wildcard: true,
+                  default_field: '*'
+                }
+              }
+            }
+          }
+        }
+      }
     }
   },
   size: 0
@@ -77,5 +132,35 @@ export const resultBuilder = (response) => ({
   },
   [propTotal]: {
     value: response.hits.total
-  }
+  },
+  [propDateHistogram]: (() => {
+    const result = {
+      labels: [],
+      [propUnaccompanied]: [],
+      [propCouplesWithoutChildren]: [],
+      [propGroupsWithoutChildren]: [],
+      [propSinglesWithChildren]: [],
+      [propFamilies]: []
+    }
+    response.aggregations[propDateHistogram].buckets.map((bucket) => {
+      const buckets = bucket[propByType].buckets
+      const label = dayjs(bucket.key).format('DD.MM.YYYY')
+
+      result.labels.push(label)
+
+      result[propUnaccompanied].push(buckets[propUnaccompanied].doc_count)
+      result[propCouplesWithoutChildren].push(
+        buckets[propCouplesWithoutChildren].doc_count
+      )
+      result[propGroupsWithoutChildren].push(
+        buckets[propGroupsWithoutChildren].doc_count
+      )
+      result[propSinglesWithChildren].push(
+        buckets[propSinglesWithChildren].doc_count
+      )
+      result[propFamilies].push(buckets[propFamilies].doc_count)
+    })
+
+    return result
+  })()
 })
