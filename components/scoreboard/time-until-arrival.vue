0bdda31d4e290ed0453fa56616b958553fe21270
gappc <gapp.christian@gmail.com>
2019-12-09 16:46:56 +0100

    Add bar chart to time-until-arrival

--- components/scoreboard/time-until-arrival.vue
+++ components/scoreboard/time-until-arrival.vue
@@ -1,11 +1,13 @@
 <template>
   <StatsContainer>
     <GaugeList :gauges="gauges"></GaugeList>
+    <BarChart :chartData="chartData" :options="chartOptions"></BarChart>
   </StatsContainer>
 </template>
 
 <script>
 import provideDataMixin from './mixins/scoreboard-data.mixin'
+import BarChart from '@/components/charts/bar.vue'
 import GaugeList from '@/components/charts/gauge-list.vue'
 import StatsContainer from '@/components/stats-container.vue'
 
@@ -14,11 +16,46 @@ import * as filters from '@/meta/filters'
 
 export default {
   components: {
+    BarChart,
     GaugeList,
     StatsContainer
   },
   mixins: [provideDataMixin(esConfig, filters.applyQueryFilters)],
   computed: {
+    chartData() {
+      if (this.metric.results == null) {
+        return null
+      }
+
+      const result = this.metric.results[0]
+      const days = result[esConfig.propDays]
+      return {
+        labels: days.map((day) => day.name),
+        datasets: [
+          {
+            data: days.map((day) => day.value),
+            fill: false,
+            borderWidth: 1
+          }
+        ]
+      }
+    },
+    chartOptions() {
+      return {
+        legend: {
+          display: false
+        },
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          yAxes: [{ ticks: { beginAtZero: true } }]
+        },
+        title: {
+          display: true,
+          text: this.metric.title
+        }
+      }
+    },
     gauges() {
       if (this.metric.results == null) {
         return []
@@ -27,16 +64,16 @@ export default {
       const result = this.metric.results[0]
       return [
         {
-          title: 'Total',
-          value: result[esConfig.propTotal].value
-        },
-        {
-          title: 'Average',
-          value: result[esConfig.propAvg].value
+          title: 'Min.',
+          value: result[esConfig.propMin]
         },
         {
           title: 'Max.',
-          value: result[esConfig.propMax].value
+          value: result[esConfig.propMax]
+        },
+        {
+          title: 'Average',
+          value: result[esConfig.propAvg]
         }
       ]
     }
--- meta/elasticsearch/time-until-arrival.js
+++ meta/elasticsearch/time-until-arrival.js
@@ -1,11 +1,22 @@
 export const propAvg = 'avg'
+export const propMin = 'min'
 export const propMax = 'max'
 export const propTotal = 'total'
+export const propDays = 'days'
 
 export const query = {
   aggs: {
     [propAvg]: {
       avg: {
+        script: {
+          inline:
+            "(doc['arrival'].date.millis - doc['submitted_on'].date.millis)/86400000",
+          lang: 'painless'
+        }
+      }
+    },
+    [propMin]: {
+      min: {
         script: {
           inline:
             "(doc['arrival'].date.millis - doc['submitted_on'].date.millis)/86400000",
@@ -21,19 +32,33 @@ export const query = {
           lang: 'painless'
         }
       }
+    },
+    [propDays]: {
+      terms: {
+        script: {
+          inline:
+            "(doc['arrival'].date.millis - doc['submitted_on'].date.millis)/86400000",
+          lang: 'painless'
+        },
+        size: 1000
+      }
     }
   },
   size: 0
 }
 
-export const resultBuilder = (response) => ({
-  [propAvg]: {
-    value: response.aggregations.propAvg.value
-  },
-  [propMax]: {
-    value: response.aggregations.propMax.value
-  },
-  [propTotal]: {
-    value: response.hits.total
+export const resultBuilder = (response) => {
+  const days = response.aggregations[propDays].buckets.map((c) => ({
+    name: parseInt(c.key, 10),
+    value: c.doc_count
+  }))
+  days.sort((a, b) => a.name - b.name)
+
+  return {
+    [propAvg]: response.aggregations[propAvg].value,
+    [propMin]: response.aggregations[propMin].value,
+    [propMax]: response.aggregations[propMax].value,
+    [propDays]: days,
+    [propTotal]: response.hits.total
   }
-})
+}
