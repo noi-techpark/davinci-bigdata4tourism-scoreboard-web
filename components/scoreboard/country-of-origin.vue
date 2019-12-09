02f2e149ce913d6f2d441b171b23d5cdfd44cb39
gappc <gapp.christian@gmail.com>
2019-12-09 11:22:17 +0100

    Add horizontal bar chart to origin

--- components/scoreboard/country-of-origin.vue
+++ components/scoreboard/country-of-origin.vue
@@ -1,24 +1,86 @@
 <template>
   <StatsContainer>
     <GaugeList :gauges="gauges"></GaugeList>
+    <HorizontalBarChart
+      :chartData="chartData"
+      :options="chartOptions"
+    ></HorizontalBarChart>
   </StatsContainer>
 </template>
 
 <script>
 import provideDataMixin from './mixins/scoreboard-data.mixin'
+import { rgbPalette, rgbaPalette } from '@/components/charts/color-util'
 import GaugeList from '@/components/charts/gauge-list.vue'
+import HorizontalBarChart from '@/components/charts/group.vue'
 import StatsContainer from '@/components/stats-container.vue'
 
 import * as esConfig from '@/meta/elasticsearch/country-of-origin'
 import * as filters from '@/meta/filters'
 
+const NUMBER_OF_TOP_COUNT = 5
+
+const percentage = (total, value) => (total !== 0 ? (100.0 / total) * value : 0)
+
+const buildTopCountries = (result) => {
+  const topCount = Math.min(
+    NUMBER_OF_TOP_COUNT,
+    result[esConfig.propTopCountries].length
+  )
+
+  return result[esConfig.propTopCountries].slice(0, topCount).map((c) => ({
+    decimalPlaces: 2,
+    title: c.name,
+    value: percentage(result[esConfig.propTotal], c.value),
+    unit: '%'
+  }))
+}
+
 export default {
   components: {
     GaugeList,
+    HorizontalBarChart,
     StatsContainer
   },
   mixins: [provideDataMixin(esConfig, filters.applyQueryFilters)],
   computed: {
+    chartData() {
+      if (this.metric.results == null) {
+        return null
+      }
+
+      const result = this.metric.results[0]
+      const topCountries = result[esConfig.propTopCountries]
+      return {
+        labels: topCountries.map((c) => c.name),
+        datasets: [
+          {
+            data: topCountries.map((c) => c.value),
+            fill: false,
+            backgroundColor: rgbaPalette(topCountries.length, 0.5),
+            borderColor: rgbPalette(topCountries.length),
+            borderWidth: 1,
+            hoverBackgroundColor: rgbaPalette(topCountries.length, 0.8)
+          }
+        ]
+      }
+    },
+    chartOptions() {
+      return {
+        legend: {
+          display: false
+        },
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          yAxes: [{ ticks: { beginAtZero: true } }]
+        },
+        title: {
+          display: true,
+          text: this.metric.title
+        }
+      }
+    },
     gauges() {
       if (this.metric.results == null) {
         return []
@@ -27,13 +89,14 @@ export default {
       const result = this.metric.results[0]
       return [
         {
-          title: 'Total',
-          value: result[esConfig.propTotal].value
+          title: 'Countries',
+          value: result[esConfig.propCountryCount]
         },
         {
-          title: 'Different countries',
-          value: result[esConfig.propCountryCount].value
-        }
+          title: 'Total',
+          value: result[esConfig.propTotal]
+        },
+        ...buildTopCountries(result)
       ]
     }
   }
--- meta/elasticsearch/country-of-origin.js
+++ meta/elasticsearch/country-of-origin.js
@@ -22,14 +22,25 @@ export const query = {
   size: 0
 }
 
-export const resultBuilder = (response) => ({
-  [propCountryCount]: {
-    value: response.aggregations[propCountryCount].value
-  },
-  [propTopCountries]: {
-    value: response.aggregations[propTopCountries].buckets
-  },
-  [propTotal]: {
-    value: response.hits.total
+export const resultBuilder = (response) => {
+  const topCountries = buildTopCountries(response)
+  const total = topCountries.reduce((prev, curr) => prev + curr.value, 0)
+
+  return {
+    [propCountryCount]: response.aggregations[propCountryCount].value,
+    [propTopCountries]: topCountries,
+    [propTotal]: total
   }
-})
+}
+
+const buildTopCountries = (response) => {
+  const result = response.aggregations[propTopCountries].buckets.map((c) => ({
+    name: c.key,
+    value: c.doc_count
+  }))
+  result.push({
+    name: 'Others',
+    value: response.aggregations[propTopCountries].sum_other_doc_count
+  })
+  return result
+}
